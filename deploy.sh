8be3e8b35d539037fc901ac690c34e9bb18001fc
Aha- <793111853@qq.com>
2014-12-26 12:02:25 +0800

    mod deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
-# install ctags, cscope
-sudo apt-install update
-sudo apt-install ctags
-sudo apt-install cscope
+# clone vimrc
+cp .vimrc .vimrc.basic .vimrc.vundle ~
 
-# vundle
+# init vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# clone my vimrc
-git clone https://github.com/Aha-/vimrc
-cd vimrc && cp .vimrc .vimrc.basic .vimrc.vundle ~
 
 # install plugins
 vim +PluginInstall +qa
