90f9c7f3e4ec16e9946a560ab3738f58b1b38d04
chenshengen <enengege@gmail.com>
2022-08-10 12:30:30 +0800

    add config of Fish Shell

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ curl -fLo ~/.config/nvim/init.vim --create-dirs \
 # curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ```
+
+## fish config
+```sh
+curl -fLo ~/.config/fish/config.fish --create-dirs https://raw.githubusercontent.com/chenen3/vimrc/master/config.fish
+```
---
+++ config.fish
@@ -0,0 +1,26 @@
+#if status is-interactive
+    # Commands to run in interactive sessions can go here
+#end
+
+set PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+set GOPATH "$(go env GOPATH)"
+set PATH "$PATH:$GOPATH/bin"
+
+# export something
+set --export GOPROXY "https://goproxy.cn,direct"
+set --export EDITOR "vim"
+
+function proxy
+	set --export -g http_proxy "http://127.0.0.1:8080"
+	set --export -g https_proxy "http://127.0.0.1:8080"
+	echo "set http_proxy to $http_proxy"
+end
+
+function unproxy
+	set --erase http_proxy
+	set --erase https_proxy
+end
+
+
+alias vi="nvim"
+alias vim="nvim"
